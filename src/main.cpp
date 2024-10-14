e0bee1dfbf5becf6c034e034dce26d8d008b69e5
RezeScarlet <gui171204@gmail.com>
2024-10-14 01:27:55 -0300

    Working and organized :3

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <imgui.h>
+#include <raylib.h>
+#include "rlImGui.h"
+#include <raylib-cpp.hpp>
+
+int main() {
+
+  SetConfigFlags(FLAG_MSAA_4X_HINT);
+  raylib::Window window(854, 480);
+  SetTargetFPS(60);
+
+  rlImGuiSetup(true);
+
+  while (WindowShouldClose() == false) {
+
+    BeginDrawing();
+    rlImGuiBegin();
+
+    ClearBackground(raylib::Color::White());
+
+    ImGui::Text("Hello, world %d", 123);
+
+    rlImGuiEnd();
+    EndDrawing();
+  }
+
+  rlImGuiShutdown();
+
+  CloseWindow();
+  return 0;
+}
